10474cd943634b2f7d24ffd473656b25c69c2b9e
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-25 19:27:17 +0530

    Added:else if statemnet to switch case

--- src/com/blz/UC/EmployeeWage.java
+++ src/com/blz/UC/EmployeeWage.java
@@ -1,27 +1,36 @@
 package com.blz.UC;
 
 public class EmployeeWage {
+    public static final int IS_PART_TIME =1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_WAGE_PER_HR = 20;
+    public static final int FULL_DAY_HR = 8;
+    public static final int PART_DAY_HR =4;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        int IS_PART_TIME =1;
-        int IS_FULL_TIME = 2;
 
-        int EMP_WAGE_PER_HR = 20;
-        int FULL_DAY_HR = 8;
-        int PART_DAY_HR =4;
-        int empWage = 0;
         int empHrs = 0;
         double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME ){
-            empHrs= FULL_DAY_HR;
-        }
-        else if (empCheck== IS_PART_TIME){
-            empHrs= PART_DAY_HR;
-        }
-        else{
-            empHrs=0;
-        }
-        empWage = empHrs * EMP_WAGE_PER_HR;
+       // if (empCheck == IS_FULL_TIME ){
+       //     empHrs= FULL_DAY_HR;
+       // }
+       // else if (empCheck== IS_PART_TIME){
+         //   empHrs= PART_DAY_HR;
+       // }
+             switch ((int)empCheck){
+                 case IS_PART_TIME:
+                     empHrs = PART_DAY_HR;
+                     break;
+                 case IS_FULL_TIME:
+                     empHrs= FULL_DAY_HR;
+                     break;
+                 default:
+                     empHrs =0;
+             }
+
+         int empWage = empHrs * EMP_WAGE_PER_HR;
         System.out.println("Emp Wage" + empWage);
+
     }
 }
