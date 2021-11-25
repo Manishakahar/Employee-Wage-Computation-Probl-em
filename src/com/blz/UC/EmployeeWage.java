d78b1b92462dd01224aa3c017a5fc67f12dd1a21
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-25 16:25:44 +0530

    [Manisha]:Added part time wage.

--- src/com/blz/UC/EmployeeWage.java
+++ src/com/blz/UC/EmployeeWage.java
@@ -4,25 +4,25 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+
         int EMP_WAGE_PER_HR = 20;
         int FULL_DAY_HR = 8;
+        int PART_TIME_HR=4;
         int empWage = 0;
         int empHrs = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == IS_FULL_TIME ){
             empHrs= FULL_DAY_HR;
-        }else{
+        }
+        else if(empCheck == IS_PART_TIME){
+            empHrs= PART_TIME_HR;
+        }
+        else{
             empHrs=0;
         }
-
         empWage = empHrs * EMP_WAGE_PER_HR;
         System.out.println("Emp Wage" + empWage);
-
-
-
-
-
-
 
     }
 }
