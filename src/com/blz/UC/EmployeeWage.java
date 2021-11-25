dcced2696cee1e30eab29b177f7fbf214f0a5f9d
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-25 21:52:40 +0530

    Added:Calculating Wages

--- src/com/blz/UC/EmployeeWage.java
+++ src/com/blz/UC/EmployeeWage.java
@@ -6,31 +6,27 @@ public class EmployeeWage {
     public static final int EMP_WAGE_PER_HR = 20;
     public static final int FULL_DAY_HR = 8;
     public static final int PART_DAY_HR =4;
+    public static final int DAYS_PER_MONTH=20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
 
-        int empHrs = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-       // if (empCheck == IS_FULL_TIME ){
-       //     empHrs= FULL_DAY_HR;
-       // }
-       // else if (empCheck== IS_PART_TIME){
-         //   empHrs= PART_DAY_HR;
-       // }
-             switch ((int)empCheck){
-                 case IS_PART_TIME:
-                     empHrs = PART_DAY_HR;
-                     break;
-                 case IS_FULL_TIME:
-                     empHrs= FULL_DAY_HR;
-                     break;
-                 default:
-                     empHrs =0;
-             }
-
-         int empWage = empHrs * EMP_WAGE_PER_HR;
-        System.out.println("Emp Wage" + empWage);
-
+        int days=1;
+        int salary=0;
+        while(days<=DAYS_PER_MONTH) {
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int) empCheck) {
+                case IS_PART_TIME:
+                    salary = (salary + (PART_DAY_HR* EMP_WAGE_PER_HR));
+                    break;
+                case IS_FULL_TIME:
+                    salary = (salary + (FULL_DAY_HR* EMP_WAGE_PER_HR));
+                    break;
+                default:
+                    salary = (salary+0);
+            }
+            days++;
+        }
+        System.out.println("Emp Wage Per Month: " + salary);
     }
 }
