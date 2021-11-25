f7af3e49d41891cfbd1aea4363453566c9a4fe9e
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-25 13:36:08 +0530

    [Manisha]:Employee is present or absent.

---
+++ src/com/blz/UC/EmployeeWage.java
@@ -0,0 +1,19 @@
+package com.blz.UC;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+       double empCheck  = Math.floor(Math.random()* 10) %2;
+        if (empCheck == 1){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
+
+
+
+
+
+    }
+}
--- src/com/blz/UC/Employeewage.java
+++
@@ -1,8 +0,0 @@
-package com.blz.UC;
-
-public class Employeewage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
-
-    }
-}
