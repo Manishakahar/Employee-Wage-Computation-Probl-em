db9cd4821cb740a7d3d1c81caadac23a393159f6
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-03 00:46:48 +0530

    Added: checking EmpWage Multiple Companies.

--- src/com/blz/empwage/EmployeeWage.java
+++ src/com/blz/empwage/EmployeeWage.java
@@ -1,19 +1,17 @@
 package com.blz.empwage;
+
 public class EmployeeWage {
     // Declaring Constants
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int FULL_DAY_HR = 8;
     public static final int PART_DAY_HR = 4;
-    public static final int EMP_WAGE_PER_HR = 20;
-    public static final int NUM_MAX_WORKING_WORKING_DAYS = 20;
-    public static final int MAX_HRS_IN_MONTH = 100;
-
     static int totalWorkingDays = 0;
     static int workingHours = 0;
     static int totalEmpHours = 0;
 
-    public static int getWorkingHours() {
+    public static int EmployeeWage(String company, int EMP_WAGE_PER_HR, int NUM_MAX_WORKING_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
+
         while (totalEmpHours < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_MAX_WORKING_WORKING_DAYS) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -21,11 +19,11 @@ public class EmployeeWage {
             switch (empCheck) {
                 case 0:
                     workingHours = FULL_DAY_HR;
-                    System.out.println("FULL DAY HR " + workingHours );
+                    System.out.println("FULL DAY HR " + workingHours);
                     break;
                 case 1:
                     workingHours = PART_DAY_HR;
-                    System.out.println("PART DAY HR" +workingHours);
+                    System.out.println("PART DAY HR" + workingHours);
                     break;
                 default:
                     workingHours = 0;
@@ -35,12 +33,14 @@ public class EmployeeWage {
         }
         //Calculated Employ wage
         int totalEmpWage = totalEmpHours * EMP_WAGE_PER_HR;
-        System.out.println("Total Emp Wage: " +totalEmpWage);
-        return  totalEmpWage;
+        System.out.println("Total Emp Wage: " + totalEmpWage);
+        return totalEmpWage;
     }
-        public static void main (String[]args){
-            System.out.println("Welcome to Employee Wage Computation");
-            getWorkingHours();
-        }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+        EmployeeWage(" DMart " + 20, 20, 20, 100);
+        EmployeeWage("Reliance " + 10, 10, 20, 100);
     }
+}
 
