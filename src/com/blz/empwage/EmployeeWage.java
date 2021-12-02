70cf8a00d43ff04a9f20b9bfd354f08158494d88
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-02 23:10:05 +0530

    Added: EmpWage Code Refactor in Class Method

--- src/com/blz/empwage/EmployeeWage.java
+++ src/com/blz/empwage/EmployeeWage.java
@@ -1,40 +1,46 @@
 package com.blz.empwage;
-
 public class EmployeeWage {
+    // Declaring Constants
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int EMP_WAGE_PER_HR = 20;
     public static final int FULL_DAY_HR = 8;
     public static final int PART_DAY_HR = 4;
-    public static final int DAYS_PER_MONTH = 20;
+    public static final int EMP_WAGE_PER_HR = 20;
+    public static final int NUM_MAX_WORKING_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
 
-    static int days = 1;
+    static int totalWorkingDays = 0;
     static int workingHours = 0;
-    static int totalHours = 0;
+    static int totalEmpHours = 0;
 
     public static int getWorkingHours() {
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        switch ((int) empCheck) {
-            case 0:
-                workingHours = FULL_DAY_HR;
-                break;
-            case 1:
-                workingHours = PART_DAY_HR;
-                break;
-            default:
-                workingHours = 0;
+        while (totalEmpHours < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_MAX_WORKING_WORKING_DAYS) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            // Calculating Wage On Employee Type
+            switch (empCheck) {
+                case 0:
+                    workingHours = FULL_DAY_HR;
+                    System.out.println("FULL DAY HR " + workingHours );
+                    break;
+                case 1:
+                    workingHours = PART_DAY_HR;
+                    System.out.println("PART DAY HR" +workingHours);
+                    break;
+                default:
+                    workingHours = 0;
+            }
+            totalEmpHours += workingHours;
+            System.out.println("Day: " + totalWorkingDays + " workingHours: " + workingHours);
         }
-        return workingHours;
+        //Calculated Employ wage
+        int totalEmpWage = totalEmpHours * EMP_WAGE_PER_HR;
+        System.out.println("Total Emp Wage: " +totalEmpWage);
+        return  totalEmpWage;
+    }
+        public static void main (String[]args){
+            System.out.println("Welcome to Employee Wage Computation");
+            getWorkingHours();
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
-        while (days <= DAYS_PER_MONTH && totalHours <= 100) {
-            totalHours = (totalHours + getWorkingHours());
-            if (getWorkingHours() != 0) {
-                days++;
-            }
-        }
-        System.out.println("Emp Wage Per Month: " + (EMP_WAGE_PER_HR * totalHours));
-    }
-}
